aa57951063855cfdeba3a46183a17c55dde471f5
arsmoriendy <nugrahasteven@gmail.com>
2025-06-02 15:42:09 +0700

    feat: dev environment nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Vial QMK development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = with pkgs; [
+          qmk
+        ];
+      };
+    };
+
+}
